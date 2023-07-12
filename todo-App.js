e920c8f7c7bcf151a51323de0790f20bccdc168c
Sergio Pinzon <Sergiopin2220@gmail.com>
2023-07-11 22:54:26 -0500

    refactorizacion metodos todo

--- todo-App.js
+++ todo-App.js
@@ -46,126 +46,94 @@ const resetAudio = (audio) => {
 // funcion para añadir tareas 
 const fullTask = []
 
-
-
-
-
-
-const createTask = ()=>{
-    // vamos a crearnos los elementos 
-    
-    const $containTask = d.createElement("div")
-    const $titleTask = d.createElement("h2")
-    const $optionTask = d.createElement("div")
-    const $editTask = d.createElement("button")
-    const $completeEditTask = d.createElement("button")
-    const $deleteTask = d.createElement("button")
-    let $idContaintTask = fullTask.length - 1;
-
-        $containTask.appendChild($titleTask)
-        $containTask.appendChild($optionTask)
-        $optionTask.appendChild($editTask)
-        $optionTask.appendChild($completeEditTask)
-        $optionTask.appendChild($deleteTask)
-    
-        $containTask.classList.add(`containt-task`)
-        $optionTask.classList.add("options")
-        $editTask.classList.add(`edite${$idContaintTask}`)
-        $editTask.classList.add(`edite-task`)
-        $completeEditTask.classList.add(`complete-edite-task`)
-        $editTask.textContent = `Editar †`
-        $completeEditTask.textContent = `Editado`
-        $completeEditTask.classList.add(`complete${$idContaintTask}`)
-        $deleteTask.textContent = `«`
-        $deleteTask.classList.add(`delete${$idContaintTask}`)
-        $deleteTask.classList.add(`delete-task`)
-        $titleTask.textContent = fullTask[fullTask.length - 1];
-        $deleteTask.dataset.delete = $idContaintTask;
-        $editTask.dataset.edite = $idContaintTask;
-        $containTask.dataset.contain = $idContaintTask;
-
-        // estilos al div contenedor de las tareas ($containTask), para organizar el contenido
-        $containTask.style.setProperty("display","flex")
-        $containTask.style.setProperty("flex-direction","column")
-        $containTask.style.setProperty("text-align","center")
-        $completeEditTask.style.setProperty("display","none");
-        // estilos al button editar
-        
-        // estilos al button eliminar
-
-    
-    console.log(fullTask)
-    console.log(fullTask.length)
-    console.log( $idContaintTask)
-    console.log( $deleteTask.dataset.delete)
-    console.log( $containTask.dataset.contain)
-    
-    $fragment.appendChild($containTask)
-    d.body.appendChild($fragment)
-
-
-    const deleteTask = ()=>{        
-        //evento para detectar si tocan el boton eliminar
-        d.addEventListener('click',(e) =>{
-            if(e.target.matches(`.delete${$idContaintTask}`) && $deleteTask.dataset.delete === $containTask.dataset.contain){
-                // alert(`esta caja es la ${$idContaintTask} a eliminar`);
-                    fullTask.splice(fullTask[$idContaintTask],1)
-                    $containTask.remove(`contain-task`)
-                    resetAudio(taskCompleteSound); //reiniciar el audio
-            }
-        })
-    }
-
-    const editeTask = ()=>{
-        // evento para detectar si tocan el boton editar
-        d.addEventListener(`click`,(e)=>{
-            if(e.target.matches(`.edite${$idContaintTask}`) && $editTask.dataset.edite === $containTask.dataset.contain){
-                // alert(`esta caja es la ${$idContaintTask} a editar`);
-                $titleTask.setAttribute("contenteditable","true")
-                if($titleTask.textContent === $titleTask.textContent){
-                    $titleTask.classList.add("editarTitle")
-                    
-                    $deleteTask.setAttribute("disabled", "true")
-                    $editTask.setAttribute("disabled", "true")
-                    $completeEditTask.style.setProperty("display","inline");
-                    if($titleTask.textContent.length <= 30 && $titleTask.textContent.length > 0){
-                        //  $titleTask.textContent = $titleTask.content
-                        d.addEventListener(`click`,(e)=>{
-                            if(e.target.matches(`.complete-edite-task`)){
-                                $titleTask.setAttribute("contenteditable","false")
-                                $titleTask.classList.remove("editarTitle")
-                                console.log($titleTask.textContent.length)
-                                $deleteTask.disabled = false;
-                                $editTask.disabled = false;
-                                $completeEditTask.style.setProperty("display","none");
-                                if($titleTask.textContent.length === 0 || $titleTask.textContent.length > 30){
-                                    $containTask.remove(`contain-task`)
-                                    console.log("")
-                                    // alert("Ingresa minimo un caracter maximo 30, pero no puede estar vacio.")
-                                }
-                            }   
-                        })
-                        
-                    }
-                    
-                    
-                }
-            }
-        })
-    }
-    editeTask()
-    deleteTask()
-}
-
-//validacion y correccion de bug con el espacio a la hora de crear tareas
-d.addEventListener("keydown", (event) => {
+const createTask = () => {
+    const $containTask = document.createElement("div");
+    const $titleTask = document.createElement("h2");
+    const $optionTask = document.createElement("div");
+    const $editTask = document.createElement("button");
+    const $completeEditTask = document.createElement("button");
+    const $deleteTask = document.createElement("button");
+    const $idContaintTask = fullTask.length - 1;
+  
+    $containTask.appendChild($titleTask);
+    $containTask.appendChild($optionTask);
+    $optionTask.appendChild($editTask);
+    $optionTask.appendChild($completeEditTask);
+    $optionTask.appendChild($deleteTask);
+  
+    $containTask.classList.add("containt-task");
+    $optionTask.classList.add("options");
+    $editTask.classList.add(`edite${$idContaintTask}`, "edite-task");
+    $completeEditTask.classList.add(`complete${$idContaintTask}`, "complete-edite-task");
+    $deleteTask.classList.add(`delete${$idContaintTask}`, "delete-task");
+    $editTask.textContent = "Editar✏️";
+    $completeEditTask.textContent = "Editado";
+    $deleteTask.textContent = "✔️"
+    $titleTask.textContent = fullTask[$idContaintTask];
+    $deleteTask.dataset.delete = $idContaintTask;
+    $editTask.dataset.edite = $idContaintTask;
+    $containTask.dataset.contain = $idContaintTask;
+  
+    $containTask.style.display = "flex";
+    $containTask.style.flexDirection = "column";
+    $containTask.style.textAlign = "center";
+    $completeEditTask.style.display = "none";
+  
+    const deleteTask = () => {
+      document.addEventListener("click", (e) => {
+        if (e.target.matches(`.delete${$idContaintTask}`) && $deleteTask.dataset.delete === $containTask.dataset.contain) {
+          fullTask.splice($idContaintTask, 1);
+          $containTask.remove();
+          resetAudio(taskCompleteSound);
+        }
+      });
+    };
+  
+    const editeTask = () => {
+      document.addEventListener("click", (e) => {
+        if (e.target.matches(`.edite${$idContaintTask}`) && $editTask.dataset.edite === $containTask.dataset.contain) {
+          $titleTask.setAttribute("contenteditable", "true");
+          $titleTask.classList.add("editarTitle");
+          $deleteTask.disabled = true;
+          $editTask.disabled = true;
+          $completeEditTask.style.display = "inline";
+  
+          if ($titleTask.textContent.length === 0 || $titleTask.textContent.length > 30) {
+            $containTask.remove();
+          }
+        }
+      });
+  
+      document.addEventListener("click", (e) => {
+        if (e.target.matches(`.complete-edite-task`)) {
+          $titleTask.setAttribute("contenteditable", "false");
+          $titleTask.classList.remove("editarTitle");
+          $deleteTask.disabled = false;
+          $editTask.disabled = false;
+          $completeEditTask.style.display = "none";
+  
+          if ($titleTask.textContent.length === 0 || $titleTask.textContent.length > 30) {
+            $containTask.remove();
+          }
+        }
+      });
+    };
+  
+    editeTask();
+    deleteTask();
+  
+    $fragment.appendChild($containTask);
+    document.body.appendChild($fragment);
+  };
+  
+  document.addEventListener("keydown", (event) => {
     if (event.target.matches(".input") && event.key === "Enter") {
       const inputValue = $input.value.trim();
   
       if (inputValue === "") {
         alert("NO SE PUEDEN MANDAR TAREAS VACIAS");
       } else if (inputValue.length <= 30) {
-        let nuevaTask = fullTask.push(inputValue);
+        fullTask.push(inputValue);
         $input.value = "";
         createTask();
       } else {
@@ -174,6 +142,7 @@ d.addEventListener("keydown", (event) => {
       }
     }
   });
+  
 
 
 //   modal
